05eca8f70ea36c3d6964cd79942a1c1e1dc7dd75
DaejunBae <dpudpu11@gmail.com>
2018-11-18 17:05:44 +0900

    Modify 상속관계 매핑 - 단일테이블 적용

--- src/main/java/com/jmt/bbang/domain/Payment.java
+++ src/main/java/com/jmt/bbang/domain/Payment.java
@@ -8,10 +8,11 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
-@Table(name = "payment")
 @Getter
 @Setter
-public class Payment {
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "DTYPE")
+public abstract class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -22,19 +23,35 @@ public class Payment {
     @Column(length = 100, nullable = false)
     private String type;
 
-    @Column(length = 100)
-    private String bankAccount;
-    @Column(length = 100)
-    private String bankName;
-
-    @Column
-    private String cardExpire;
-    @Column(length = 100)
-    private String cardNum;
 
     @Column(columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime regdate;
     @OneToOne(cascade = CascadeType.REMOVE)
     @JoinColumn(name="purchase_id")
     private Purchase purchase;
+}
+
+@Entity
+@DiscriminatorValue("C")
+class Card extends Payment{
+    @Column
+    private String cardExpire;
+    @Column(length = 100)
+    private String cardNum;
+    @Column
+    private String cardHolder;
+    @Column
+    private String cardCompany;
+}
+
+
+@Entity
+@DiscriminatorValue("B")
+class Bank extends Payment{
+    @Column(length = 100)
+    private String bankAccount;
+    @Column(length = 100)
+    private String bankName;
+    @Column
+    private String remitter;
 }
